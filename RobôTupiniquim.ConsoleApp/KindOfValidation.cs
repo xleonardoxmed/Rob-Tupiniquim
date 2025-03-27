6281863d48150ef558bb828905b26850f0921c24
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-27 16:17:43 -0300

    Add project files.

---
+++ RobôTupiniquim.ConsoleApp/KindOfValidation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobôTupiniquim.ConsoleApp
+{
+    class KindOfValidation
+    {
+        public static int area;
+        public static void AreaValidation()
+        {
+            bool success;
+            int width = 0;
+            int heigth = 0;
+            int answer = 0;
+            string ask = "";
+
+            for (int checks = 0; checks <= 1; checks++)
+            {
+                if (checks == 0)
+                {
+                    ask = "LARGURA";               
+                }
+                else
+                {
+                    ask = "ALTURA";             
+                }
+                do
+                {
+                    Console.Write($"{ask}: ");
+                    string entry = Console.ReadLine()!;
+                    success = int.TryParse(entry, out answer);
+
+                    if (!success || answer <= 0)
+                    {
+                        Console.WriteLine("Erro! Somente números inteiros e positivos serão aceitos.");
+                        Console.WriteLine("---------------------------------------------------------------------------");
+                        continue;
+                    }
+
+                } while (!success || answer <= 0);
+
+                if (checks == 0)
+                    width = answer;
+                else
+                    heigth = answer;
+            }
+            area = width * heigth;
+            Console.WriteLine($"\nA área escolhida é de: {area}m²");
+        }
+        public static void ComandValidation()
+        {
+
+        }
+
+    }
+}
---
+++ RobôTupiniquim.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace RobôTupiniquim.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=======================ROBÔ TUPINIQUIM=====================================");
+                Console.WriteLine("A exploração do cosmos é uma viagem para autodescoberta.");
+                Console.WriteLine("===========================================================================");
+                Console.WriteLine("Escolha o a LARGURA e a ALTURA do terreno retangular que deseja explorar: ");
+                Console.WriteLine("---------------------------------------------------------------------------");
+
+                KindOfValidation.AreaValidation();
+
+                Console.WriteLine("\n===========================================================================");
+                Console.Write("Deseja explorar uma nova áera? (S/N): ");
+                Console.WriteLine("\n===========================================================================");
+                string toContinue = Console.ReadLine()!.ToUpper();
+
+                if (toContinue != "S")
+                    break;
+            }
+        }
+    }
+}
