b5bb678429adaac1e7378edde3f7a790b1203d25
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-29 20:23:26 -0300

    Validação de comandos implementada. Agora o usuário pode inserir o que é pedido de várias formas diferentes e o programa está preparado para ler e filtrar.

--- RobôTupiniquim.ConsoleApp/KindOfValidation.cs
+++ RobôTupiniquim.ConsoleApp/KindOfValidation.cs
@@ -9,11 +9,13 @@ using System.Threading.Tasks;
 namespace RobôTupiniquim.ConsoleApp
 {
     class KindOfValidation
-    {   
+    {
+        public static int chars;
         public static int Xcoordenate;
         public static int Ycoordenate;
         public static char direction;
         public static int area;
+        public static char[] allChars;
         public static void AreaValidationAndDraw()
         {
             bool success;
@@ -55,12 +57,13 @@ namespace RobôTupiniquim.ConsoleApp
             area = width * heigth;
             Console.WriteLine($"\n                    A área escolhida é de: {area}m²");
             Console.WriteLine("---------------------------------------------------------------------------");
-            Console.WriteLine("\n                        Exemplo ilustrativo: ");
-            Console.WriteLine("                           ------------");
-            Console.WriteLine("                          |            |");
-            Console.WriteLine($"                          |            | {heigth}m");
-            Console.WriteLine("                          |            |");
-            Console.WriteLine("                           ------------");
+            Console.WriteLine("\n                       Exemplo ilustrativo:\n ");
+            Console.WriteLine($"                              (X,Y)");
+            Console.WriteLine($"                  (0,{heigth})  ------------  ({width}, {heigth})");
+            Console.WriteLine("                         |            |");
+            Console.WriteLine($"                         |            | {heigth}m");
+            Console.WriteLine("                         |            |");
+            Console.WriteLine($"                  (0,0)   ------------  ({width}, 0)");
             Console.WriteLine($"                               {width}m");
             Console.WriteLine("---------------------------------------------------------------------------");
             Console.WriteLine("                 Digite qualquer tecla para continuar");
@@ -72,7 +75,7 @@ namespace RobôTupiniquim.ConsoleApp
         public static void AskValidCommands()
         {
             String[] locationAndDirection;
-            String[] movements;
+            //String[] movements;
             int robot;
             bool success = false;
 
@@ -89,8 +92,9 @@ namespace RobôTupiniquim.ConsoleApp
                 do
                 {
                     Console.Write($"\nInsira a COORDENADA INICIAL do Robô #{robot} (X,Y) e a DIREÇÃO (N,S,L, O): ");
-
-                    locationAndDirection = Console.ReadLine()!.Split(' ');
+                    string entry = Console.ReadLine()!;
+                    locationAndDirection = entry.Split(new char[] { ' ', ',', '(', ')'}, StringSplitOptions.RemoveEmptyEntries);
+                    //REMOVE TODOS OS CARACTERES INDESEJADOS E FILTRA O QUE É PEDIDO
 
                     if (locationAndDirection.Length != 3)
                     {
@@ -101,7 +105,7 @@ namespace RobôTupiniquim.ConsoleApp
                         Console.WriteLine("---------------------------------------------------------------------------");
                         continue;
                     }
-                    
+
                     success =
                         int.TryParse(locationAndDirection[0], out Xcoordenate) &&
                         int.TryParse(locationAndDirection[1], out Ycoordenate) &&
@@ -111,7 +115,7 @@ namespace RobôTupiniquim.ConsoleApp
                     if (!success || Xcoordenate < 0 || Ycoordenate < 0)
                     {
                         Console.WriteLine("---------------------------------------------------------------------------");
-                        Console.WriteLine("Erro! Insira comandos válidos separados por espaços. EX: (Número Número Letra).");
+                        Console.WriteLine("Erro! Insira comandos válidos separados por espaços (SEM VÍRGULA). EX: (Número Número Letra).");
                         Console.WriteLine("---------------------------------------------------------------------------");
                         Console.WriteLine("Apenas números inteiros permitidos");
                         continue;
@@ -124,10 +128,21 @@ namespace RobôTupiniquim.ConsoleApp
                 do
                 {
                     Console.Write($"\nInsira os MOVIMENTOS do Robô #{robot} (E, D M): ");
-                    movements = Console.ReadLine()!.Split(' ');
 
-                    success = movements.All(m => "EDM".Contains(char.ToUpper(m[0])));
+                    string allMovements = Console.ReadLine()!.ToUpper();
+                    allChars = allMovements.Where(m => m != ' ' && m != '-' && m != ',').ToArray();
 
+
+                    for (int charChecks = 0; charChecks < allChars.Length; charChecks++)
+                    {
+                        if (allChars[charChecks] != 'E' && allChars[charChecks] != 'D' && allChars[charChecks] != 'M' || int.TryParse(allChars[charChecks].ToString(), out int _))
+                        {
+                            success = false;
+                            break;
+                        }
+                        // '_' SERVE PARA VARIÁVEIS DESCARTÁVEIS // TryParse espera uma string, pra isso: ToString();           
+                    }
+               
                     if (!success)
                     {
                         Console.WriteLine("---------------------------------------------------------------------------");
